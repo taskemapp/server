8d2c820385a816d9a72ab2853b0457d811c54503
ripls56 <84716344+ripls56@users.noreply.github.com>
2024-07-17 17:53:08 +0300

    test(jwt, auth): add some tests
    Add jwt token creation unit test
    Two integration auth test `Login` `SignUp`

---
+++ test/grpc_test.go
@@ -0,0 +1,58 @@
+package test
+
+import (
+	"context"
+	"fmt"
+	"github.com/brianvoe/gofakeit/v7"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"taskem/internal/config"
+	v1 "taskem/tools/gen/grpc/v1"
+	"testing"
+)
+
+var (
+	cfg, _       = config.New()
+	testEmail    = gofakeit.Email()
+	testPassword = gofakeit.Password(true, true, true, true, false, 8)
+)
+
+func TestSignUp(t *testing.T) {
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	}
+
+	conn, err := grpc.NewClient(fmt.Sprint("0.0.0.0:", 50051), opts...)
+	defer conn.Close()
+
+	require.Nil(t, err)
+
+	client := v1.NewAuthClient(conn)
+
+	_, err = client.SignUp(context.Background(), &v1.SignupRequest{
+		UserName: gofakeit.Username(),
+		Email:    testEmail,
+		Password: testPassword,
+	})
+	require.Nil(t, err)
+}
+
+func TestLogin(t *testing.T) {
+	conn, err := grpc.NewClient(
+		fmt.Sprintf("0.0.0.0:%d", cfg.GrpcPort),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	defer conn.Close()
+
+	require.Nil(t, err)
+
+	client := v1.NewAuthClient(conn)
+
+	resp, err := client.Login(context.Background(), &v1.LoginRequest{
+		Email:    testEmail,
+		Password: testPassword,
+	})
+	require.Nil(t, err)
+	t.Log(resp)
+}
---
+++ test/jwt_test.go
@@ -0,0 +1,91 @@
+package test
+
+import (
+	"github.com/brianvoe/gofakeit/v7"
+	jwt2 "github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+	"taskem/internal/pkg/jwt"
+	"testing"
+	"time"
+)
+
+func TestNewToken(t *testing.T) {
+	tests := []struct {
+		name        string
+		opts        jwt.Opts
+		failed      bool
+		expectedErr error
+	}{
+		{
+			name: "",
+			opts: jwt.Opts{
+				Email:    gofakeit.Email(),
+				Secret:   "secret",
+				Duration: time.Second * 10,
+				Id:       uuid.New(),
+			},
+			failed:      true,
+			expectedErr: jwt.ErrDuration,
+		},
+		{
+			name: "",
+			opts: jwt.Opts{
+				Email:    gofakeit.Email(),
+				Secret:   "secret",
+				Duration: time.Minute,
+				Id:       uuid.New(),
+			},
+			failed: false,
+		},
+		{
+			name: "",
+			opts: jwt.Opts{
+				Email:    gofakeit.Email(),
+				Secret:   "secret",
+				Duration: time.Hour,
+			},
+			failed: false,
+		},
+		{
+			name: "",
+			opts: jwt.Opts{
+				Email:    gofakeit.Email(),
+				Secret:   "",
+				Duration: time.Hour,
+			},
+			failed:      true,
+			expectedErr: jwt.ErrMissingSecret,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			token, err := jwt.NewToken(test.opts)
+
+			if !test.failed {
+				require.Nil(t, err)
+				parsed, err := jwt2.Parse(token, func(token *jwt2.Token) (interface{}, error) {
+					if _, ok := token.Method.(*jwt2.SigningMethodHMAC); !ok {
+						t.Fatalf("unexpected signing method: %v", token.Header["alg"])
+					}
+
+					return []byte(test.opts.Secret), nil
+				})
+				if claims, ok := parsed.Claims.(jwt2.MapClaims); ok {
+					require.Equal(t, test.opts.Id.String(), claims["uid"])
+					require.Equal(t, test.opts.Email, claims["email"])
+				} else {
+					t.Fatalf("failed get claims from token: %s", token)
+				}
+				require.Nil(t, err)
+			} else {
+				if test.expectedErr != nil {
+					require.ErrorIs(t, err, test.expectedErr)
+				}
+			}
+		})
+	}
+}

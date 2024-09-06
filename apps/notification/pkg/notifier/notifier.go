0cab2bd3f897980fc5cb7129a7d613b831d35bfc
ripls56 <84716344+ripls56@users.noreply.github.com>
2024-09-06 14:58:17 +0300

    chore(notifier): imprv reusability

--- apps/notification/pkg/notifier/notifier.go
+++ apps/notification/pkg/notifier/notifier.go
@@ -1,13 +1,27 @@
 package notifier
 
 const (
-	EmailNotify = "email"
-	PushNotify  = "push"
+	ChannelEmail = "email.notify"
+	ChannelPush  = "push.notify"
 )
 
 type Notification struct {
-	Type      string `json:"type"`
-	Recipient string `json:"recipient"`
-	Subject   string `json:"subject"`
-	Message   string `json:"message"`
+	Title   string `json:"title"`
+	Message string `json:"message"`
+}
+
+type EmailNotification struct {
+	Notification
+	To   string `json:"to"`
+	From string `json:"from"`
+}
+
+type PushNotification struct {
+	Notification
+	Tokens   []string               `json:"tokens"`
+	Topic    string                 `json:"topic,omitempty"`
+	Image    string                 `json:"image,omitempty"`
+	Subtitle string                 `json:"subtitle,omitempty"`
+	Priority int                    `json:"priority,omitempty"`
+	Data     map[string]interface{} `json:"data,omitempty"`
 }

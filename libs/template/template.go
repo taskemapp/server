e140020a440d4f58441a2c3f8579c5350262142c
ripls56 <84716344+ripls56@users.noreply.github.com>
2024-09-09 19:42:05 +0300

    chore(template): imrv api
    - Changed err wrap with uber like style
    - Added `options` now template can be configured if this needed
    - Documented some code

--- libs/template/template.go
+++ libs/template/template.go
@@ -7,15 +7,31 @@ import (
 	"path/filepath"
 )
 
-type Template int
+// Type represent template type
+//
+// Example:
+// template.VerifyEmailTemplate
+type Type int
 
 const (
-	VerifyEmailTemplate Template = iota
+	VerifyEmailTemplate Type = iota + 1
 )
 
 const (
 	verifyEmailName = "verify_email.gohtml"
 )
+
+type option struct {
+	dir string
+}
+
+type OptionFn func(*option)
+
+func WithDir(dir string) OptionFn {
+	return func(o *option) {
+		o.dir = dir
+	}
+}
 
 type VerifyEmail struct {
 	Name             string
@@ -23,9 +39,20 @@ type VerifyEmail struct {
 	UnsubscribeLink  string
 }
 
-func Get(t Template) (*template.Template, error) {
+// Get retrieves and parses the specified template.
+// It accepts the TemplateType type and the variable number OptionFn to set the template's directory.
+// If no directory is specified, the current working directory is used by default.
+func Get(t Type, opts ...OptionFn) (*template.Template, error) {
 	wd, err := os.Getwd()
-	path := filepath.Join(wd, "templates")
+	defaultOpt := option{dir: wd}
+	for _, opt := range opts {
+		if opt == nil {
+			continue
+		}
+		opt(&defaultOpt)
+	}
+
+	path := filepath.Join(defaultOpt.dir, "templates")
 	if err != nil {
 		return nil, err
 	}
@@ -35,7 +62,7 @@ func Get(t Template) (*template.Template, error) {
 		path = filepath.Join(path, verifyEmailName)
 		file, err := template.ParseFiles(path)
 		if err != nil {
-			return nil, errors.Wrap(err, "Failed to parse verify email template")
+			return nil, errors.Wrap(err, "verify email")
 		}
 		return file, nil
 	default:
